b9cbfde1e40f9c486d7717f29f283f6e57a55142
Mitchelle M <shanellmusarurwa6@gmail.com>
2024-04-12 22:15:25 +0200

    imported font awesome

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { GlobalContextProvider } from "./context/global";
+import GlobalStyle from "./Gloabalstyle";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GlobalStyle />
+    <GlobalContextProvider>
+      <App />
+    </GlobalContextProvider>
+  </React.StrictMode>
+);
